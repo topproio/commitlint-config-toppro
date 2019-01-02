f2a368959bbf412f739a7df1afdbdceeb8bd732e
houqian <houqian@weilaigongzuo.com>
2019-01-02 16:21:23 +0800

    test: add

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,7 @@
-module.exports = {
-	
-	rules: {
-		"type-empty": [2, "never"], //type必填
-		"type-case": [2, "always", "lowerCase"], //type小写
-		"type-enum": [2, "always", ["feat", "fix", "refactor", "chore", "docs", "test", "optimize", "build", "revert", "merge"]],
-		"header-max-length": [2, "always", 50], //header最大长度
-		"scope-max-length": [2, "always", 10], //作用域长度
-		"scope-case": [2, "always", 'lowerCase'], //scope小写
-		"body-max-length": [2, "always", 75], //body最大长度
-		"body-leading-blank": [2, "always"], //body是在新的一行写
-		"footer-max-line-length": [2, "always", 50], //footer最大长度
-		"footer-leading-blank": [2, "always"], //footer是在新的一行写
 
-		"subject-empty": [1, "never"] //subject必填
-	},
-	parserPreset: {
-	    parserOpts: {
-	      	headerPattern: /^(\w+)(?:\((.*)\))?(:\s(.*))?$/,
-	      	headerCorrespondence: ['type', 'scope', 'subject']
-	    }
-	}
+const rules = require('./index');
+console.log(rules);
+module.exports = {
+	rules: rules.rules
 }
 
